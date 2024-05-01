44a2818aca7bb6229358f97dec95ddd5926ff127
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-01 20:23:45 +0530

    navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+   <>
+        <nav className='w-full h-14 bg-slate-500 flex items-center justify-center'>
+            <p className='text-xl text-white font-semibold'>Employee Dashboard</p>
+        </nav>
+   </>
+  )
+}
+
+export default NavBar
