dc505afe0f2d0a0e5b712ffdd8e7b851f86c5619
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-02 15:56:22 +0530

    not found route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Components/Home'
 import EmployeeDetails from './Components/EmployeeDetails'
+import NotFound from './Components/NotFound'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<EmployeeDetails/>} />
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
 
