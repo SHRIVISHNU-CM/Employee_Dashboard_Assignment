b0a5d3ec6a260d8cb868e36b251a1207897480db
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-02 15:55:36 +0530

    display center of screen

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Loader() {
   return (
     <>
-        <div class="flex justify-center gap-2">
+        <div class="flex justify-center items-center w-full gap-2 h-screen">
                 <div class="w-3 h-3 rounded-full animate-pulse bg-blue-800"></div>
                 <div class="w-3 h-3 rounded-full animate-pulse bg-blue-800"></div>
                 <div class="w-3 h-3 rounded-full animate-pulse bg-blue-800"></div>
