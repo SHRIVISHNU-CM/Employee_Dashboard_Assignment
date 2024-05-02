e7cf4f1dfe216c21909c0d7abc5b0503d8c7db8a
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-02 15:55:58 +0530

    Not found file

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+   <>
+    <p className='text-2xl font-semibold text-blue-500'>NOT FOUND</p>
+   </>
+  )
+}
+
+export default NotFound
