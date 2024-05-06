bda443fcb9374f5c6bb8f7a021c64dea9536673d
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-06 22:13:44 +0530

    suspense lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NotFound from './Components/NotFound'
-import Skeleton from './Components/Skeleton'
+
 const EmployeeDetails = lazy(() => import("./Components/EmployeeDetails"))
 const Home = lazy(() => import("./Components/Home"))
-
+import Loader from './Components/Loader'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Suspense fallback={<Skeleton />}>
-          <Route path="/" element={<Home />} />
-        </Suspense>
-        <Suspense fallback={<Skeleton />}>
-          <Route path="/:id" element={<EmployeeDetails />} />
+        
+        <Route path="/" element={<Home />} />
 
-        </Suspense>
+        <Route path="/:id" element={<EmployeeDetails />} />
+
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
